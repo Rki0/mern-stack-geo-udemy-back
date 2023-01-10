b86d17ae55a5bbfd0584d7dbc87262d4a972aa7e
Rki0 <pky00823@gmail.com>
2023-01-10 11:09:39 +0900

    express basic

--- app.js
+++ app.js
@@ -1,32 +1,18 @@
-const http = require("http");
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const server = http.createServer((req, res) => {
-  console.log("INCOMING REQUEST");
-  console.log(req.method, req.url);
+const app = express();
 
-  if (req.method === "POST") {
-    let body = "";
+app.use(bodyParser.urlencoded({ extended: false }));
 
-    req.on("end", () => {
-      const userName = body.split("=")[1];
-      res.end(`<h1>${userName}</h1>`);
-    });
-
-    req.on("data", (chunk) => {
-      body += chunk;
-    });
-  } else {
-    // res.end로 전달하는 것이 html 태그가 아니라 그냥 평문이라는 것을 알려주고자 하는 것
-    // res.setHeader("Content-Type", "text/plain");
-
-    // text/html을 적어주면 html로 작동함.
-    res.setHeader("Content-Type", "text/html");
-
-    // res.end("<h1>Success!</h1>");
-    res.end(
-      '<form method="POST"><input type="text" name="username" /><button type="submit">Create User</button></form>'
-    );
-  }
+app.post("/user", (req, res, next) => {
+  res.send(`<h1>User: ${req.body.username}</h1>`);
 });
 
-server.listen(5000);
+app.get("/", (req, res, next) => {
+  res.send(
+    '<form action="/user" method="POST"><input type="text" name="username" /><button type="submit">POST</button></form>'
+  );
+});
+
+app.listen(5000);
